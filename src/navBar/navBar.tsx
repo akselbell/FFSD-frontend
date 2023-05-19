291cc0ffec8084ea3ffa05b33d3064b56e013365
Aksel Bell <aksel.bell@duke.edu>
2023-05-19 19:16:33 -0400

    Started navbar

--- src/navBar/navBar.tsx
+++ src/navBar/navBar.tsx
@@ -4,10 +4,13 @@ import "./navBar.css";
 function Navbar() {
     return (
     <div className="navbar">
-        <div> {/*left side */}
-            <div>
-
+        <div className="left-navbar"> {/*left side */}
+            <div id="ffsd">
+                FFSD
             </div>
+            {[{name: "About"}, {name: "Resources"}, {name: "Events"}, {name: "Impact"}, {name: "Sponsors"}].map((word, index) => { //add link field for each bracket
+                return <a key={index}>{word.name}</a>;
+            })}
         </div> 
         <div> {/*right side */}
 
