a14cc525a86a0f50649756341e0ae5e784d19347
Pascal Bell <pascal.bell@duke.edu>
2023-05-31 12:07:24 -0400

    logout/login/signup made better

--- src/join/join.tsx
+++ src/join/join.tsx
@@ -2,49 +2,75 @@ import React from "react";
 import "./join.css";
 import Header from "../navBar/header";
 import Navbar from "../navBar";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
 
 function Join() {
+    const [res, setResponse] = useState(null);
+    const [error, setError] = useState(null);
+
+    const fetchData = async () => {
+        const username = (document.getElementById("uname") as HTMLInputElement).value;
+        const password = (document.getElementById("password") as HTMLInputElement).value;
+        const email = (document.getElementById("email") as HTMLInputElement).value;
+        try {
+            const response = await fetch("/api/signup", {   //hits the backend, fetches from localhost:80/api/login and passes that information
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    username: username,
+                    password: password,
+                    email: email
+                })
+            });
+
+            const responseData = await response.json();
+            if (responseData.message) {
+                console.log(responseData.message);
+                setResponse(responseData.message);
+            }
+            if (responseData.error) {
+                console.log(responseData.error);
+                setError(responseData.error);
+            }
+        } catch (error) {
+            console.log(error);
+            return null;
+        }
+    };
+
     return <>
         <Header />
         <Navbar />
         <div>
-            <form className="joinForm" onSubmit={(e) => {
-                e.preventDefault();                             //prevents clearing the form for a new entry
-                const username = (document.getElementById("uname") as HTMLInputElement).value;
-                const password = (document.getElementById("password") as HTMLInputElement).value;
-                const email = (document.getElementById("email") as HTMLInputElement).value;
-                
-                fetch("/api/signup", {   //hits the backend, fetches from localhost:80/api/login and passes that information
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json"
-                    },
-                    body: JSON.stringify({
-                        username: username,
-                        password: password,
-                        email: email
-                    })
-                }).then((response) => {
-                    console.log(response.status);
+            {res ? 
+            <div>
+                {res}
+                <br/>
+                <Link className="logIn" to="/login">Log In</Link>
+            </div> :
+            <>
+                <form className="joinForm" onSubmit={(e) => {
+                    e.preventDefault();                             //prevents clearing the form for a new entry   
+                    fetchData();   
+                }}>
+                    <label htmlFor="uname"><b>Username: </b></label>
+                    <input id="uname" type="text" placeholder="Type your username" name="uname" required />
 
-                    if(response.ok) {
-                        return <div>
-                            Thank you for signing up, please log in!
-                        </div>;
-                    }
-                });
-            }}>
-                <label htmlFor="uname"><b>Username: </b></label>
-                <input id="uname" type="text" placeholder="Type your username" name="uname" required />
+                    <label htmlFor="pass"><b>Password: </b></label>
+                    <input id="password" type="password" placeholder="Type your password" name="pass" required />
 
-                <label htmlFor="pass"><b>Password: </b></label>
-                <input id="password" type="password" placeholder="Type your password" name="pass" required />
+                    <label htmlFor="email"><b>Email: </b></label>
+                    <input id="email" type="email" placeholder="Type your email" name="email" required />
 
-                <label htmlFor="email"><b>Email: </b></label>
-                <input id="email" type="email" placeholder="Type your email" name="email" required />
-
-                <input type="submit" value="send" />
-            </form>
+                    <input type="submit" value="send" />
+                </form>
+                <div>{error}</div>
+            </>
+            }
         </div>
     </>;
 }
--- src/login/login.tsx
+++ src/login/login.tsx
@@ -1,37 +1,112 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./login.css";
 import Header from "../navBar/header";
 import Navbar from "../navBar";
+import { useState } from "react";
 
 function LogIn() {
+    const [loggedIn, setLoggedIn] = useState(false);
+    const [error, setError] = useState(null);
+
+    const fetchData = async () => {
+        const username = (document.getElementById("uname") as HTMLInputElement).value;
+        const password = (document.getElementById("password") as HTMLInputElement).value;
+        try {
+            const response = await fetch("/api/login", {   //hits the backend, fetches from localhost:80/api/login and passes that information
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    username: username,
+                    password: password,
+                })
+            });
+
+            const responseData = await response.json();
+            
+            if (responseData.message) setLoggedIn(true);
+            if (responseData.error) {
+                setLoggedIn(false);
+                setError(responseData.error);
+            }
+        }  catch (error) {
+            console.log(error);
+            return null;
+        }
+    };
+
+    useEffect(() => {
+        const checkStatus = async () => {
+            try {
+                const response = await fetch("/api/login", {
+                    method: "GET",
+                    headers: {
+                        "Content-Type": "application/json"
+                    }
+                });
+                const responseData = await response.json();
+                if (responseData.message) setLoggedIn(true);
+                if (responseData.error) {
+                    setLoggedIn(false);
+                    setError(responseData.error);
+                }
+            } catch (error) {
+                console.log(error);
+                return null;
+            }
+        };
+        checkStatus();
+    }, []);
+
+    const logOut = async () => {
+        try {
+            const response = await fetch("/api/signout", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            });
+            const responseData = await response.json();
+            if (responseData.message) setLoggedIn(false);
+            if (responseData.error) {
+                setError(responseData.error);
+                setLoggedIn(true);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
     return <>
         <Header />
         <Navbar />
         <div>
-            <form className="login" onSubmit={(e) => {
-                e.preventDefault();                             //prevents clearing the form for a new entry
-                const username = (document.getElementById("uname") as HTMLInputElement).value;
-                const password = (document.getElementById("password") as HTMLInputElement).value;
+            {loggedIn ?
+                <div>
+                    <div>Login sucessfull!</div>
+                    <button onClick={() => {
+                        logOut();
+                    }}>logout</button>
+                </div>
+                :
+                <div>
+                    <form className="login" onSubmit={(e) => {
+                        e.preventDefault();                             //prevents clearing the form for a new entry
+                        fetchData();
+                    }}>
+                        <label htmlFor="uname"><b>Username: </b></label>
+                        <input id="uname" type="text" placeholder="Type your username" name="uname" required />
+    
+                        <label htmlFor="pass"><b>Password: </b></label>
+                        <input id="password" type="password" placeholder="Type your password" name="pass" required />
+    
+                        <input type="submit" value="send" />
+                    </form>
+                    <div>{error}</div>
+                </div>
                 
-                fetch("/api/login", {   //hits the backend, fetches from localhost:80/api/login and passes that information
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json"
-                    },
-                    body: JSON.stringify({
-                        username: username,
-                        password: password,
-                    })
-                });
-            }}>
-                <label htmlFor="uname"><b>Username: </b></label>
-                <input id="uname" type="text" placeholder="Type your username" name="uname" required />
-
-                <label htmlFor="pass"><b>Password: </b></label>
-                <input id="password" type="password" placeholder="Type your password" name="pass" required />
-
-                <input type="submit" value="send" />
-            </form>
+            }
         </div>
     </>;
 }
