d297fb6a07e71292088a0f0d3f1b9a3b815f8f3e
Pascal Bell <pascal.bell@duke.edu>
2023-06-04 17:09:10 -0400

    login css started

--- src/login/login.tsx
+++ src/login/login.tsx
@@ -34,30 +34,33 @@ function LogIn() {
                     }}>logout</button>
                 </div>
                 :
-                <div>
-                    <form className="login" onSubmit={(e) => {
-                        e.preventDefault();                             //prevents clearing the form for a new entry
-                        const username = (document.getElementById("uname") as HTMLInputElement).value;
-                        const password = (document.getElementById("password") as HTMLInputElement).value;
-                        
-                        login(username, password).then(userVal => {
-                            if(typeof userVal === "string") return console.error(userVal);
-                            dispatch(setUser(userVal));
-                        });
+                <div className="backgroundLogin">
+                    <div className="loginContainer">
+                        <form className="login" onSubmit={(e) => {
+                            e.preventDefault();                             //prevents clearing the form for a new entry
+                            const username = (document.getElementById("uname") as HTMLInputElement).value;
+                            const password = (document.getElementById("password") as HTMLInputElement).value;
+                            
+                            login(username, password).then(userVal => {
+                                if(typeof userVal === "string") return console.error(userVal);
+                                dispatch(setUser(userVal));
+                            });
 
-                    }}>
-                        <label htmlFor="uname"><b>Username: </b></label>
-                        <input id="uname" type="text" placeholder="Type your username" name="uname" required />
-    
-                        <label htmlFor="pass"><b>Password: </b></label>
-                        <input id="password" type="password" placeholder="Type your password" name="pass" required />
-    
-                        <input type="submit" value="send" />
-                    </form>
-                    {/*<div>{error}</div>*/}
+                        }}><div className="loginTitle">Login</div>
+                            <label htmlFor="uname"><b>Username: </b></label>
+                            <input id="uname" type="text" placeholder="Type your username" name="uname" required />
+        
+                            <label htmlFor="pass"><b>Password: </b></label>
+                            <input id="password" type="password" placeholder="Type your password" name="pass" required />
+        
+                            <input type="submit" value="send" />
+                        </form>
+                        {/*<div>{error}</div>*/}
+                    </div>
                 </div>
                 }
         </div>
+        <div className="footerLogin"></div>
     </>;
 }
 
