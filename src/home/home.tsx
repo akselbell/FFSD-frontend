8ff3d16a120683562a2584aa6bd6367cfbcf8e3c
Aksel Bell <aksel.bell@duke.edu>
2023-06-15 23:37:11 -0400

    edited landing page

--- src/home/home.tsx
+++ src/home/home.tsx
@@ -4,9 +4,9 @@ import Header from "../navBar/header";
 import Resource from "./resources";
 import "./home.css";
 
-const resources = [{image: "https://images.unsplash.com/photo-1606811841689-23dfddce3e95?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8Mnx8fGVufDB8fHx8&w=1000&q=80", alt: "resource1", title: "Resource #1", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."},
-    {image: "https://images.unsplash.com/photo-1606811841689-23dfddce3e95?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8Mnx8fGVufDB8fHx8&w=1000&q=80", alt: "resource2", title: "Resource #2", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."},
-    {image: "https://images.unsplash.com/photo-1606811841689-23dfddce3e95?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8Mnx8fGVufDB8fHx8&w=1000&q=80", alt: "resource3", title: "Resource #3", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."}
+const resources = [{image: "../Rectangle 100.jpg", alt: "Practice Management", title: "Practice Management", text: "Templates for weekly staff meetings, job-based to-do lists, and office checklists"},
+    {image: "../Rectangle 100-2.jpg", alt: "Human Resources", title: "Human Resources", text: "Job descriptions, phone scripts, and more"},
+    {image: "../Rectangle 100-3.jpg", alt: "Marketing", title: "Marketing", text: "Social media posts, monthly patient emails, and new patient materials"}
 ];
 
 function Home() {
@@ -39,7 +39,13 @@ function Home() {
             </div>
         </div>
         <div className="frame25">
-            Resources
+            <div className="resourcesHeader">
+                Resources
+            </div>
+            <div className="resourcesTxt">
+            Access our Resource Library to find resources tailored specifically to fee-for-service dentistry. <br></br>
+            Stay ahead of the curve and elevate your dentistry to new heights
+            </div>
             <div className="Resources">
                 {resources.map((word, index) => {
                     return <Resource image={word.image} alt={word.alt} title={word.title} text={word.text} key={index}/>;
