a1fa1588a069458aa621d34616b6a8805c4a514b
Pascal Bell <pascal.bell@duke.edu>
2023-06-16 16:57:01 -0400

    forgot pass & reset pass done

--- src/forgotPass/forgotPass.tsx
+++ src/forgotPass/forgotPass.tsx
@@ -3,34 +3,46 @@ import { forgotPass } from "../store/user";
 import "./forgotPass.css";
 import React, { useState } from "react";
 import { Alert } from '@mui/material';
+import { Link } from "react-router-dom";
+import Header from "../navBar/header";
+import Navbar from "../navBar";
 
 function ForgotPass() {
     const [error, setError] = useState(undefined); 
     const [message, setMessage] = useState(undefined);
 
     return <>
-        <form className="login" onSubmit={(e) => {
-            e.preventDefault();
-            const email = (document.getElementById("email") as HTMLInputElement).value;
-            forgotPass(email).then(v => {
-                if (v.error) {
-                    setError(v.error); 
-                    setMessage(undefined);
-                }
-                else if (v.message) {
-                    setMessage(v.message);
-                    setError(undefined);
-                }
-            }).catch(error => console.log(error));
-        }}>
-            <div className="loginTitle">Forgot Password</div>
-                <label htmlFor="email">Email: </label>
-                <input id="email" type="text" name="email" required />
+        <Header/>
+        <Navbar/>
+        <div className="backgroundLogin">
+            <div className="loginContainer">
+                <form className="login" onSubmit={(e) => {
+                    e.preventDefault();
+                    const email = (document.getElementById("email") as HTMLInputElement).value;
+                    forgotPass(email).then(v => {
+                        if (v.error) {
+                            setError(v.error); 
+                            setMessage(undefined);
+                        }
+                        else if (v.message) {
+                            setMessage(v.message);
+                            setError(undefined);
+                        }
+                    }).catch(error => console.log(error));
+                }}>
+                    <div className="loginTitle">Forgot Password?</div>
+                        <div className="forgotPassTxt">Enter your email to receive a link to reset your password.</div>
+                        <label htmlFor="email" className="login-label">Email</label>
+                        <input className="login-input" id="email" type="text" name="email" required />
 
-                <input className="loginButton" type="submit" value="Submit" />
-                {error && <Alert severity="error">{error}</Alert>}
-                {message && <Alert severity="success">{message}</Alert>}
-        </form>
+                        <input className="loginButton" type="submit" id="join" value="Submit" />
+                        <div className="notMember">Remembered your password? <Link className="joinLogin" to="/login">Login</Link></div>
+                        {error && <Alert severity="error">{error}</Alert>}
+                        {message && <Alert severity="success">{message}</Alert>}
+                </form>
+            </div>
+        </div>
+        <div className="footer"></div>
     </>;
 }
 
--- src/forgotPass/resetPass.tsx
+++ src/forgotPass/resetPass.tsx
@@ -4,32 +4,48 @@ import "./resetPass";
 import React, { useState } from "react";
 import { useLocation } from "react-router-dom";
 import { Alert } from "@mui/material";
+import Navbar from "../navBar";
+import Header from "../navBar/header";
 
 function ResetPass() {
-    const [error, setError] = useState(undefined);
+    const [error, setError] = useState("");
     const [message, setMessage] = useState(undefined);
 
     const { token } = queryString.parse(useLocation().search) as any; 
     return <>
-        <form className="login" onSubmit={(e) => {
-            e.preventDefault();
-            const password = (document.getElementById("pass") as HTMLInputElement).value;
-            
-            resetPass(token, password).then(v => {
-                if (v.error) setError(v.error);
-                if(v.message) setMessage(v.message);
-            }).catch((error) => console.log(error));
-        }}>
-            <div className="loginTitle">Reset Password</div>
-                <label htmlFor="pass">New Password: </label>
-                <input id="pass" type="password" name="pass" required />
+        <Header/>
+        <Navbar/>
+        <div className="backgroundLogin">
+            <div className="loginContainer">
+                <form className="login" onSubmit={(e) => {
+                    e.preventDefault();
+                    const password = (document.getElementById("pass") as HTMLInputElement).value;
+                    const confirmPass = (document.getElementById("confirmPass") as HTMLInputElement).value;
+                    setError("");
+                    setMessage(undefined);
+                    if (password === confirmPass) {
+                        resetPass(token, password).then(v => {
+                            if (v.error) setError(v.error);
+                            if(v.message) setMessage(v.message);
+                        }).catch((error) => console.log(error));
+                        return;
+                    }
+                    setError("Passwords do not match!");
+                }}>
+                    <div className="loginTitle">Reset Password</div>
+                        <label htmlFor="pass" className="login-label">New Password</label>
+                        <input className="login-input" id="pass" type="password" name="pass" required />
 
-                {/*add this twice and see if they match*/}
+                        <label htmlFor="confirmPass" className="login-label">Confirm Password</label>
+                        <input className="login-input" id="confirmPass" type="password" name="pass" required />
 
-                <input className="loginButton" type="submit" value="Submit" />
-                {error && <Alert severity="error">{error}</Alert>}
-                {message && <Alert severity="success">{message}</Alert>}
-        </form>
+                        <input className="loginButton" type="submit" value="Submit" />
+                        {error && <Alert severity="error">{error}</Alert>}
+                        {message && <div id="PassResetSent"><Alert severity="success">{message}</Alert></div>}
+                </form>
+            </div>
+        </div>
+        <div className="footer"/>
     </>;
 }
 
