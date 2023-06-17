97e5c9687b4981a2a63c3f2bda30b74f4533cbcd
Aksel Bell <aksel.bell@duke.edu>
2023-06-17 19:04:06 -0400

    added pay now to nav bar

--- src/navBar/navBar.tsx
+++ src/navBar/navBar.tsx
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { login, logout, setUser, userStateT } from "../store/user";
 import { portalButton } from "../store/user";
+import PayNowButton from "../join/payNow";
 
 function Navbar() {
 
@@ -11,8 +12,8 @@ function Navbar() {
     const user = useSelector((s: {user_state: userStateT}) => s.user_state.user);//this gets the state from user.ts
 
     return <>
-        {user?.valid_subscription ?  
-            <div className="navbar">
+        {user ?  
+            user?.valid_subscription ? <div className="navbar"> {/*if logged in and paid */}
                 <div className="navbar-cont"> {/*left side */}
                     <Link className="ffsdButton" to="/"> {/*put button inside here */}
                         <div id="ffsd">
@@ -38,9 +39,37 @@ function Navbar() {
                         </Link>
                     </div>
                 </div> 
+            </div> :
+            <div className="navbar">   {/*if logged in but not paid */}
+                <div className="navbar-cont"> {/*left side */}
+                    <Link className="ffsdButton" to="/"> {/*put button inside here */}
+                        <div id="ffsd">
+                            FFSD
+                        </div>
+                    </Link>
+                    <div className="txtNavbar">
+                        {[{name: "About", href: "/about"}, {name: "Events", href: "/events"}, {name: "News", href: "/news"}, {name: "Get Involved", href: "/getinvolved"}, {name: "Advocacy", href: "/advocacy"}].map((word, index) => {
+                            return <Link className="txtNavbar" to={word.href} key={index}>{word.name}</Link>;
+                        })}
+                    </div>
+                    <div className="right-navbar"> {/*right side */}
+                        <div className="logIn">
+                            <span onClick={() => {window.location.href = "/join";}}>Pay Now</span>
+                        </div>
+                        <Link className="logIn" id="logout" to="/">
+                            <span onClick={() => {
+                                dispatch(logout());
+                                window.location.href = "/login";
+                            }}>
+                                    Logout
+                            </span>
+                        </Link>
+                    </div>
+                </div> 
             </div>
+            
         : 
-            <div className="navbar">
+            <div className="navbar">  {/* not logged in */}
                 <div className="navbar-cont"> {/*left side */}
                     <Link className="ffsdButton" to="/"> {/*put button inside here */}
                         <div id="ffsd">
