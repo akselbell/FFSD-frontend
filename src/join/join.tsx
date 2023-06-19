042647108349f895d1de0a1d94d0022e5d4efaf2
Pascal Bell <pascal.bell@duke.edu>
2023-06-18 21:07:29 -0400

    join page done

--- src/join/join.tsx
+++ src/join/join.tsx
@@ -62,29 +62,68 @@ function Join() {
                             console.log(err);
                         });  
                     }}>
-                        <div className="loginTitle">Join Now</div>
+                        <div className="loginTitle" id="joinTitle">Join Now</div>
                         <div className="forgotPassTxt" id="joinTxt">Join the Fee for Service Dentist Association today and become part of a dynamic network of dental professionals dedicated to elevating dentistry to new heights. Gain access to exclusive resources, educational programs, and a supportive community that will propel your practice forward. </div>
                         <div className="joinRow">
-                            <label htmlFor="prefix"><b>Prefix</b></label>
-                            <input id="prefix" type="text" name="prefix"/>
-
-                            <label htmlFor="firstName"><b>First Name *</b></label>
-                            <input id="firstName" name="firstName" required />
-
-                            <label htmlFor="lastName"><b>Last Name *</b></label>
-                            <input id="lastName" type="text" name="lastName" required />
+                            <div className="joinField">
+                                <label className="login-label" htmlFor="prefix"><b>Prefix</b></label>
+                                <input className="login-input" id="prefix" type="text" name="prefix"/>
+                            </div>
+                            <div className="joinField">
+                                <label className="login-label" htmlFor="firstName"><b>First Name *</b></label>
+                                <input className="login-input" id="firstName" name="firstName" required />
+                            </div>
+                            <div className="joinField">
+                                <label className="login-label" htmlFor="lastName"><b>Last Name *</b></label>
+                                <input className="login-input" id="lastName" type="text" name="lastName" required />
+                            </div>
                         </div>
                         <div className="joinRow">
-                            <label htmlFor="uname"><b>Username: </b></label>
-                            <input id="uname" type="text" placeholder="Type your username" name="uname" required />
-
-                            <label htmlFor="pass"><b>Password: </b></label>
-                            <input id="password" type="password" placeholder="Type your password" name="pass" required />
-
-                            <label htmlFor="email"><b>Email: </b></label>
-                            <input id="email" type="email" placeholder="Type your email" name="email" required />
+                            <div className="joinField">
+                                <label className="login-label" htmlFor="practiceName"><b>Practice Name *</b></label>
+                                <input className="login-input" id="practiceName" type="text" name="practiceName"/>
+                            </div>
+                            <div className="joinField">
+                                <label className="login-label" htmlFor="email"><b>Email Address *</b></label>
+                                <input className="login-input" id="email" type="email" name="email" required />
+                            </div>
                         </div>
-                        <input type="submit" value="send" />
+                        <div className="joinRow">
+                            <div className="joinField">
+                                <label className="login-label" htmlFor="practiceAddress"><b>Practice Address *</b></label>
+                                <input className="login-input" id="practiceAddress" type="text" name="practiceAddress"/>
+                            </div>
+                            <div className="joinField">
+                                <label className="login-label" htmlFor="apt"><b>Apt. or Suite # *</b></label>
+                                <input className="login-input" id="apt" type="text" name="apt" />
+                            </div>
+                        </div>
+                        <div className="joinRow">
+                            <div className="joinField">
+                                <label className="login-label" htmlFor="city"><b>City *</b></label>
+                                <input className="login-input" id="city" type="text" name="city"/>
+                            </div>
+                            <div className="joinField">
+                                <label className="login-label" htmlFor="postalCode"><b>Postal Code *</b></label>
+                                <input className="login-input" id="postalCode" name="postalCode" required />
+                            </div>
+                            <div className="joinField">
+                                <label className="login-label" htmlFor="state"><b>State *</b></label>
+                                <input className="login-input" id="state" type="text" name="state" required />
+                            </div>
+                        </div>
+
+                        {/*<div className="joinRow">  {/*this is username password and email fields, should be arranged accordingly 
+                            <div className="joinField">
+                                <label className="login-label" htmlFor="uname"><b>Username</b></label>
+                                <input className="login-input" id="uname" type="text" name="uname" required />
+                            </div>
+                            <div className="joinField">
+                                <label className="login-label" htmlFor="pass"><b>Password</b></label>
+                                <input className="login-input" id="password" type="password" name="pass" required />
+                            </div>
+                        </div>*/}
+                        <input className="loginButton" id="joinSubmit" type="submit" value="Submit" />
 
                         {error && <Alert severity="error">{error}</Alert>}
                     </form>
